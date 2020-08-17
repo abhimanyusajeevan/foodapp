d5222f041a42a2e0919db8345f83cab0ff2f0996
abhimanyusajeevan <abhimanyusajeevan@gmail.com>
2020-08-17 21:21:30 +0530

    version with order field final1

--- foodproj/settings.py
+++ foodproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0t37u$t@m7ek1*mlw4m!#5kiry(a7utzzr28gx-xsdtj&%@)+^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
